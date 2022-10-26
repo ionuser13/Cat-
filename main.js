687c62569501f8b47adca87f899f94675073f9cd
userjohn <johnchacpi479@gmail.com>
2022-10-26 14:19:00 -0500

    added new api link

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-console.log("Hello World");
-const API = "https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_armx4JrLwsJuAo9OitmUn4ieHibSokktW9YTlE6Du6q6InUtMOArwesfWhg3Sjfk";
+const API_Main = "https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_armx4JrLwsJuAo9OitmUn4ieHibSokktW9YTlE6Du6q6InUtMOArwesfWhg3Sjfk";
+const API_Fav = "https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_armx4JrLwsJuAo9OitmUn4ieHibSokktW9YTlE6Du6q6InUtMOArwesfWhg3Sjfk";
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
-const img3 = document.getElementById("img3");
-async function reload(){
-    const response = await fetch(API);
+async function loadRandomCats(){
+    const response = await fetch(API_Main);
     const data = await response.json()
     console.log(data)
     img1.src = data[0].url;
     img2.src = data[1].url;
-    img3.src = data[2].url;
 }
-reload()
+
+async function loadFavoritesCats() {
+
+}
+loadRandomCats()
