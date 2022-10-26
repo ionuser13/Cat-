fff6d0ae5f05faf7d7a229685ef57b352105efa7
userjohn <johnchacpi479@gmail.com>
2022-10-25 23:08:57 -0500

    added 2 images

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 console.log("Hello World");
-const API = "https://api.thecatapi.com/v1/images/search"
-fetc
+const API = "https://api.thecatapi.com/v1/images/search?limit=3";
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
+const img3 = document.getElementById("img3");
+async function reload(){
+    const response = await fetch(API);
+    const data = await response.json()
+    console.log(data)
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+}
+reload()
